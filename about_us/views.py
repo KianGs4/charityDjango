c856f1c12b46494b2bedb772242b804feb012ca8
KianGs4 <kian.ghassemi1383@gmail.com>
2023-08-01 16:40:39 +0330

    added about_us page

---
+++ about_us/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from accounts.models import User
+
+
+def about_us(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'about_us.html', context=context)
